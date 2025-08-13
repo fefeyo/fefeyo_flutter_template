7d73c541446c48af6965cd0019fea8342b57a766
sakatayu <yu.sakata@hacomono.co.jp>
2025-08-14 01:00:39 +0900

    CI setup

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,10 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:fefeyo_flutter_template/core/router/app_router.gr.dart';
 
-/**
- * ルーティング設定するページ一覧
- */
-import '../../features/home/home_page.dart';
+/// ルーティング設定するページ一覧
 
 @AutoRouterConfig(replaceInRouteName: 'Page,Route')
 class AppRouter extends RootStackRouter {
--- lib/features/home/view_model/home_view_model.dart
+++ lib/features/home/view_model/home_view_model.dart
@@ -2,11 +2,10 @@ import 'package:fefeyo_flutter_template/features/home/data/home_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final StateNotifierProvider<HomeViewModel, HomeState> homeViewModelProvider =
-StateNotifierProvider<HomeViewModel, HomeState>((
-    StateNotifierProviderRef<HomeViewModel, HomeState> ref) {
+    StateNotifierProvider<HomeViewModel, HomeState>((Ref ref) {
   return HomeViewModel();
 });
 
 class HomeViewModel extends StateNotifier<HomeState> {
-  HomeViewModel() : super(HomeState());
+  HomeViewModel() : super(const HomeState());
 }
